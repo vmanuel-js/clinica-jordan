6aec3d139d89746db803d86e68ddacdeb9f5bc9d
Manuel Jordan <vmjordansolis@gmail.com>
2026-06-13 23:51:08 -0500

    feat: initial ASP.NET Core MVC project setup

---
+++ ClinicaJordan/ClinicaJordan.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClinicaJordan.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
