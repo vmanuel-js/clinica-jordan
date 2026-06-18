9c0e6b550d04c4597dc7c596890f82dd4438d0d9
Manuel Jordan <vmjordansolis@gmail.com>
2026-06-17 21:06:29 -0500

    feat: complete the CRUD of Medico

--- ClinicaJordan/ClinicaJordan.Web/Controllers/MedicoController.cs
+++ ClinicaJordan/ClinicaJordan.Web/Controllers/MedicoController.cs
@@ -1,16 +1,19 @@
 ﻿using ClinicaJordan.Web.Data;
 using ClinicaJordan.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ClinicaJordan.Web.Controllers
 {
     public class MedicoController : Controller
     {
         private readonly MedicoRepository _medicoRepository;
+        private readonly EspecialidadRepository _especialidadRepository;
 
-        public MedicoController(MedicoRepository medicoRepository)
+        public MedicoController(MedicoRepository medicoRepository, EspecialidadRepository especialidadRepository)
         {
             _medicoRepository = medicoRepository;
+            _especialidadRepository = especialidadRepository;
         }
 
         public IActionResult Index()
@@ -21,6 +24,7 @@ namespace ClinicaJordan.Web.Controllers
 
         public IActionResult Insertar()
         {
+            ViewBag.Especialidades = new SelectList(_especialidadRepository.Listar(), "IdEspecialidad", "Nombre");
             return View();
         }
 
@@ -28,6 +32,31 @@ namespace ClinicaJordan.Web.Controllers
         public IActionResult Insertar(Medico medico)
         {
             _medicoRepository.Insertar(medico);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Actualizar(string dni)
+        {
+            var medicos = _medicoRepository.ObtenerPorDni(dni);
+            ViewBag.Especialidades = new SelectList(_especialidadRepository.Listar(), "IdEspecialidad", "Nombre");
+            return View(medicos);
+        }
+
+        [HttpPost]
+        public IActionResult Actualizar(Medico medico)
+        {
+            _medicoRepository.Actualizar(medico);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Desactivar(string dni)
+        {
+            var resultado = _medicoRepository.Desactivar(dni);
+
+            if (resultado != "OK")
+                TempData["Error"] = resultado;
+            else
+                TempData["Exito"] = "Médico desactivado correctamente";
             return RedirectToAction("Index");
         }
     }
--- ClinicaJordan/ClinicaJordan.Web/Data/MedicoRepository.cs
+++ ClinicaJordan/ClinicaJordan.Web/Data/MedicoRepository.cs
@@ -57,10 +57,80 @@ namespace ClinicaJordan.Web.Data
             cmd.Parameters.AddWithValue("@telefono", medico.Telefono);
             cmd.Parameters.AddWithValue("@turno", medico.Turno);
             cmd.Parameters.AddWithValue("@id_especialidad", medico.IdEspecialidad);
-            cmd.Parameters.AddWithValue("@fecha_nacimiento", medico.FechaNacimiento);
+            cmd.Parameters.AddWithValue("@fecha_nacimiento", medico.FechaNacimiento.ToDateTime(TimeOnly.MinValue));
 
             cnx.Open();
             cmd.ExecuteNonQuery();
+        }
+
+        public Medico? ObtenerPorDni(string dni)
+        {
+            using var cnx = new SqlConnection(_connectionString);
+            using var cmd = new SqlCommand("SP_ObtenerMedicoPorDni", cnx);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@dni", dni);
+            cnx.Open();
+
+            using var reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                return new Medico
+                {
+                    IdMedico = Convert.ToInt32(reader["IdMedico"]),
+                    Dni = reader["Dni"].ToString()!,
+                    Nombres = reader["Nombres"].ToString()!,
+                    Apellidos = reader["Apellidos"].ToString()!,
+                    Correo = reader["Correo"].ToString()!,
+                    Telefono = reader["Telefono"].ToString()!,
+                    Turno = reader["Turno"].ToString()!,
+                    IdEspecialidad = Convert.ToInt32(reader["IdEspecialidad"]),
+                    FechaNacimiento = DateOnly.FromDateTime(Convert.ToDateTime(reader["FechaNacimiento"])),
+                    Estado = Convert.ToChar(reader["Estado"])
+                };
+            }
+            return null;
+        }
+
+        public void Actualizar(Medico medico)
+        {
+            using var cnx = new SqlConnection(_connectionString);
+            using var cmd = new SqlCommand("SP_ActualizarMedico", cnx);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@dni", medico.Dni);
+            cmd.Parameters.AddWithValue("@nombres", medico.Nombres);
+            cmd.Parameters.AddWithValue("@apellidos", medico.Apellidos);
+            cmd.Parameters.AddWithValue("@correo", medico.Correo);
+            cmd.Parameters.AddWithValue("@telefono", medico.Telefono);
+            cmd.Parameters.AddWithValue("@turno", medico.Turno);
+            cmd.Parameters.AddWithValue("@id_especialidad", medico.IdEspecialidad);
+            cmd.Parameters.AddWithValue("@fecha_nacimiento", medico.FechaNacimiento.ToDateTime(TimeOnly.MinValue));
+            cmd.Parameters.AddWithValue("@estado", medico.Estado);
+
+            cnx.Open();
+            cmd.ExecuteNonQuery();
+        }
+
+        public string Desactivar(string dni)
+        {
+            var medico = ObtenerPorDni(dni);
+
+            if (medico == null)
+                return $"El médico con el DNI: {dni} no existe.";
+            if (medico.Estado == 'I')
+                return "El médico ya se encuentra inactivo";
+
+            using var cnx = new SqlConnection(_connectionString);
+            using var cmd = new SqlCommand("SP_DesactivarMedico", cnx);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@dni", dni);
+
+            cnx.Open();
+            cmd.ExecuteNonQuery();
+
+            return "OK";
         }
     }
 }
